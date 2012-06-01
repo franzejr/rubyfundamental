a8b93d2a5954542898709ee7375b365fe8d81ecb
Franzé Jr <franzejr@gmail.com>
2012-06-01 15:31:11 -0300

    get image

--- get_image.rb
+++ get_image.rb
@@ -1,5 +1,13 @@
-require 'open-uri'
+#Get Image from an Dynamic URL
+require 'net/http'
 
-open('image.jpg', 'wb') do |file|
-  file << open('http://maps.googleapis.com/maps/api/staticmap?center=Berkeley,CA&zoom=14&size=400x400&sensor=false').read
+uri = URI('http://maps.googleapis.com/maps/api/staticmap?')
+params = { :center => 'Berkeley,CA', :zoom => 14, :size => "400x400", :sensor => false }
+uri.query = URI.encode_www_form(params)
+
+res = Net::HTTP.get_response(uri)
+puts "success" if res.is_a?(Net::HTTPSuccess)
+
+open('image2222.png', 'wb') do |file|
+  file << res.body
 end
