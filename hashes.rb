3fb3e065e92733483c868e0d6fdc699883a53c7b
Franzé Jr <franzejr@gmail.com>
2012-07-03 16:35:57 -0300

    [#master] Refactoring the hashes file

--- hashes.rb
+++ hashes.rb
@@ -6,39 +6,16 @@
     hash = {"numJumps" => @numJumps,"allFollowing"=>@allFollowing,"numCities"=>@numCities}
     maximum = hash.values.max()
 
-    puts "Hash Inicial: "
+    puts "Initial hash: "
     puts hash
 
-    puts "O maior elemento eh: " + hash.key(maximum).to_s
+    puts "The largest element is: " + hash.key(maximum).to_s + " and your value is: " + maximum.to_s
 
-    puts "Retirando o maior elemento do hash"
-
-    hash_antigo = {}
-
-    key_max =  hash.key(maximum)
-
-    puts "Key ox max element" + key_max.to_s
-
-    hash_antigo[key_max] = maximum
-
-    puts "hash antigo:"
-    puts hash_antigo
-
-    hash.delete_if{ |key,value| value == maximum }
-
-    puts "Apos tirar o maior elemento"
-    puts hash
-
-    #Dividindo todos os elementos pelo maior
-    hash.each { |key,value|  hash[key] = value/maximum  }
+    puts "Updating the elements"    
+    hash.each do |key,value|
+        hash[key] = value/maximum if hash[key] != maximum
+        
+    end
     
-    
-    puts "Imprimindo apos dividir"
-    puts hash
-    
-    puts "Adicionando agora o elemento maior"
-    
-    hash.merge!(hash_antigo)
-    
-    puts "Adicionando o elemento cujo maior elemento"
+    puts "New hash: "
     puts hash
