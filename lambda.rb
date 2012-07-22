cafc70cce08b8b88f8f453d0c41acf9ee62f1359
Franzé Jr <franzejr@gmail.com>
2012-07-22 19:26:21 -0300

    lambda examples

--- lambda.rb
+++ lambda.rb
@@ -1,7 +1,10 @@
+#Criando uma lambda function
 somar = lambda {|x,y| x+y}
 
+#Criando com o Proc.
 somar2 = Proc.new {|x,y| x+y} 
 
+#Mandandoa imprimir, entao a convencao eh de que eh melhor usar do/end ao inves de { }
 somar3 = lambda do |x,y|
    x+y
    puts x+y 
